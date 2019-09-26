3a909cc6a64b726255baf907678035453e7f2ccc
ctemari <50718656+ctemari@users.noreply.github.com>
2019-09-26 17:33:40 -0400

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -4,6 +4,6 @@ resource "azurerm_resource_group" "myterraformgroup" {
     location = "eastus"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "Terraform CPQ"
     }
 }
